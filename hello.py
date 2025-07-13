c509ac617c136caf9b5a347352d9020ff198b0fe
Mxer12 <egorglot12@gmail.com>
2025-07-14 00:04:44 +0700

    hello1

--- hello.py
+++ hello.py
@@ -1,35 +1,10 @@
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+import telebot
 
-# Токен вашего бота (замените на ваш токен)
-TOKEN = "8072440393:AAHJloXf5KWoL5sZXLbcIpwmB2Da_xaEUuU"
+token = '8072440393:AAHJloXf5KWoL5sZXLbcIpwmB2Da_xaEUuU'
+bot = telebot.TeleBot(token)
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Отправляет приветственное сообщение при команде /start."""
-    update.message.reply_text('Привет! Отправь мне любое сообщение, и я отвечу.')
+@bot.message_handler(content_types=['text'])
+def start(message):
+    bot.send_message(message.chat.id, message.text)
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Отвечает тем же текстом, что отправил пользователь."""
-    user_message = update.message.text
-    update.message.reply_text(f'Вы написали: "{user_message}". Вот мой ответ!')
-
-def main() -> None:
-    """Запуск бота."""
-    # Создаем Updater и передаем ему токен бота
-    updater = Updater(TOKEN)
-
-    # Получаем диспетчер для регистрации обработчиков
-    dispatcher = updater.dispatcher
-
-    # Регистрируем обработчики команд и сообщений
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Запускаем бота
-    updater.start_polling()
-
-    # Останавливаем бота при нажатии Ctrl+C
-    updater.idle()
-
-if name == 'main':
-    main()
+bot.infinity_polling()
