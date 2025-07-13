8d48cb480b2eb436d69de2dd75067b84ac054f5f
Mxer12 <egorglot12@gmail.com>
2025-07-13 20:23:54 +0700

    Update button.js

--- button.js
+++ button.js
@@ -5,7 +5,7 @@ const bot = new Telegraf('8072440393:AAHJloXf5KWoL5sZXLbcIpwmB2Da_xaEUuU');
 bot.start((ctx) => {
     const button = Markup.button.webApp(
         "✨ Открыть мини-приложение",
-        "https://myusername.github.io/tg-mini-app/"
+        "https://mxer12.github.io/TelegramMiniBot/"
     );
     ctx.reply(
         "Нажмите кнопку, чтобы открыть мини-приложение:",
